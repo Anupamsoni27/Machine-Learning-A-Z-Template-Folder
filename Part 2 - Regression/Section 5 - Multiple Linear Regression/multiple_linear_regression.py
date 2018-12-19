5f01bc378269fd01c98d70ae83930361e156cedd
Anupam Soni <anupamsoni27@gmail.com>
2018-12-19 15:14:53 +0530

    Files added

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -4,4 +4,42 @@ Created on Wed Dec 19 11:11:43 2018
 
 @author: anupam.soni
 """
+#Data Preprocessing Template
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+df = pd.read_csv(r"C:\Anupam\Machine Learning A-Z Template Folder\Part 2 - Regression\Section 5 - Multiple Linear Regression\50_Startups.csv")
+
+X = df.iloc[:,:-1].values
+Y = df.iloc[:,4].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the dummy variable Trap
+X = X[:,1:]
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test,Y_train, Y_test = train_test_split(X,Y,test_size = 0.2, random_state  =0)
+
+
+
+#Feature Scalling
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)"""
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,Y_train)
+
+y_pred = regressor.predict(X_test)
